6e2397d564336f058c117de109a6275d0923ccb2
DarkLight0418 <samseong0418@naver.com>
2026-01-22 19:12:29 +0900

    20260122 rust: 명시적으로 모듈로 선언된 코드만 컴파일 단위에 포함됨

--- rust_part/src/main.rs
+++ rust_part/src/main.rs
@@ -1,3 +1,8 @@
+use std::env::var;
+
+mod var1;
+
 fn main() {
     println!("Please run 'rustfmt!'");
+    var1::var1();
 }
---
+++ rust_part/src/var1.rs
@@ -0,0 +1,5 @@
+pub fn var1() {
+    let mut x = 1;
+    x = 3;
+    println!("{}", x);
+}

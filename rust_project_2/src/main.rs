e1bffe8a5f2afed4bc4443d03d7c612b758d6c64
DarkLight0418 <samseong0418@naver.com>
2026-01-26 13:04:06 +0900

    20260126 여러개의 값 리턴, 리턴값 없을 때

--- rust_project_2/src/main.rs
+++ rust_project_2/src/main.rs
@@ -1,4 +1,7 @@
+use crate::swap::swap;
+
 mod add;
+mod swap;
 
 const THRESHOLD: i32 = 10;
 
@@ -6,8 +9,19 @@ fn is_big(n: i32) -> bool {
     n > THRESHOLD
 }
 
+fn do_nothing() -> () {
+    return ();
+}
+
+fn me_too() {}
+
 fn main() {
     println!("{}", THRESHOLD);
     println!("{}", is_big(5));
     println!("{}", add::add(1, 2));
+    let (num1, num2) = swap(1, 2);
+    println!("{num1}, {num2}");
+
+    println!("{:?}", do_nothing());
+    println!("{:?}", me_too());
 }
---
+++ rust_project_2/src/swap.rs
@@ -0,0 +1,3 @@
+pub fn swap(num1: i32, num2: i32) -> (i32, i32) {
+    (num2, num1)
+}

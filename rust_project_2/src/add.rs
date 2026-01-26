7805a9d158421ee6b76e019b499c350247f37512
DarkLight0418 <samseong0418@naver.com>
2026-01-26 09:27:40 +0900

    20260126 함수 테스트

---
+++ rust_project_2/src/add.rs
@@ -0,0 +1,9 @@
+pub fn add(num1: i32, num2: i32) -> i32 {
+    return num1 + num2;
+}
+
+/*
+  fn add(num1: i32, num2: i32) -> i32 {
+    num1 + num2;
+  }
+*/
--- rust_project_2/src/main.rs
+++ rust_project_2/src/main.rs
@@ -1,3 +1,5 @@
+mod add;
+
 const THRESHOLD: i32 = 10;
 
 fn is_big(n: i32) -> bool {
@@ -7,4 +9,5 @@ fn is_big(n: i32) -> bool {
 fn main() {
     println!("{}", THRESHOLD);
     println!("{}", is_big(5));
+    println!("{}", add::add(1, 2));
 }

43bf2b068733abc8e8bb5f33395d8daed5e23b8e
DarkLight0418 <samseong0418@naver.com>
2026-06-16 14:52:18 +0900

    [study] 2026-06-16: match문 연습

---
+++ Rust_Programming_Book/test_20260616/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let name = "John";
+    match name {
+        "John" => println!("Hello, John!"),
+        "Mary" => println!("Hello, Mary!"),
+        _ => println!("Hello, My Teacher!"),
+    }
+}
